f9e6c77167f0bd74f36a8b6526e41701e372539a
Gustavo Saboia <gustavosaboia97@hotmail.com>
2019-04-16 19:15:12 -0300

    Default mongo URI

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGO_DB_URI = process.env.MONGO_DB_URI
+const MONGO_DB_URI = process.env.MONGO_DB_URI || 'mongodb://localhost:27017/music'
 
 mongoose.connect(MONGO_DB_URI, { useNewUrlParser: true })
 
