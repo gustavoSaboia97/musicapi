9cdad14a2bfb9525a89c90cf10646f88187e62d8
Gustavo Saboia <gustavosaboia97@hotmail.com>
2019-04-29 00:02:02 -0300

    Changed the routes method to support error handler

--- src/routes/author-routes.js
+++ src/routes/author-routes.js
@@ -13,13 +13,25 @@ router.get(urlBuilder.AUTHOR, authenticator.authenticate, (req, resp, next) => {
                     .catch( error => next(error))
 })
 
-router.post(urlBuilder.AUTHOR, authenticator.authenticate, (req, resp) => authorController.addAuthor(req, resp))
+router.post(urlBuilder.AUTHOR, authenticator.authenticate, (req, resp, next) => {
+    authorController.addAuthor(req, resp)
+                    .catch( error => next(error))
+})
 
-router.get(urlBuilder.AUTHOR_ID, authenticator.authenticate, (req, resp) => authorController.getAuthor(req, resp))
+router.get(urlBuilder.AUTHOR_ID, authenticator.authenticate, (req, resp, next) => {
+    authorController.getAuthor(req, resp)
+                    .catch(error => next(error))
+})
 
-router.put(urlBuilder.AUTHOR_ID, authenticator.authenticate, (req, resp) => authorController.editAuthor(req, resp))
+router.put(urlBuilder.AUTHOR_ID, authenticator.authenticate, (req, resp, next) => {
+    authorController.editAuthor(req, resp)
+                    .catch( error => next(error))
+})
 
-router.delete(urlBuilder.AUTHOR_ID, authenticator.authenticate, async (req, resp) => await authorController.deleteAuthor(req, resp))
+router.delete(urlBuilder.AUTHOR_ID, authenticator.authenticate, (req, resp, next) => {
+    authorController.deleteAuthor(req, resp)
+                    .catch( error => next(error))
+})
 
 
 export default router
