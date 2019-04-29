fbe7d24541ca91cdd2003da5be7d556cfd8bd3f3
Gustavo Saboia <gustavosaboia97@hotmail.com>
2019-04-28 22:28:26 -0300

    Insert of author data
    - Database communication to insert new author data

--- test/unit/src/repository/author-repository.spec.js
+++ test/unit/src/repository/author-repository.spec.js
@@ -5,16 +5,29 @@ import AuthorRepository from '../../../../src/repository/author-repository'
 jest.mock('../../../../src/config/mongodb');
 
 let authorRepository = new AuthorRepository()
+let mockedFunctions = {
+    save : () => {}
+}
 
 beforeEach(() => {
     MongoAuthor.mockClear()
 });
 
-it('Should get all authors', () => {
+it('Should get all authors from database', () => {
 
     let response = authorRepository.getAuthors()
 
     expect(MongoAuthor.find).toHaveBeenCalled()
+})
+
+it('Should add new author to database', async () => {
+    let author = new Author('Name', [])
+
+    MongoAuthor.mockReturnValue(mockedFunctions)
+
+    let response = await authorRepository.insertAuthor(author)
+
+    expect(MongoAuthor).toHaveBeenCalled()
 })
 
 it('Should get author by id', async () => {
@@ -25,12 +38,3 @@ it('Should get author by id', async () => {
 
     expect(MongoAuthor.findById).toHaveBeenCalled()
 })
-
-// it('Should delete author by id', async () => {
-
-//     let id = "id"
-
-//     let response = await authorRepository.deleteAuthor(id)
-
-//     expect(MongoAuthor.findByIdAndRemove).toHaveBeenCalled()
-// })
